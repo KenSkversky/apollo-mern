fe985a7022427932f30c9d9f0a385dc4422e5f38
KenSkversky <kenneth.skversky@gmail.com>
2021-04-05 00:34:03 -0400

    Added Signup and Password Hashing

--- src/index.js
+++ src/index.js
@@ -1,58 +1,99 @@
 const {ApolloServer, gql} = require('apollo-server');
 const MongoClient = require('mongodb').MongoClient;
+const bcrypt = require('bcryptjs');
 
 const dotenv = require('dotenv');
 dotenv.config();
 
-dotenv.DB_NAME
 const {DB_URI, DB_NAME} = process.env;
-// Dummy Data
-const books = [
-    {
-      title: 'The Awakening',
-      author: 'Kate Chopin',
-    },
-    {
-      title: 'City of Glass',
-      author: 'Paul Auster',
-    },
-  ];
 
-
-  
-  // A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
-// TypeDefs are wrapped inside ```````` back ticks
 const typeDefs = gql`
-# The "Query" type is special: it lists all of the available queries that
-# clients can execute, along with the return type for each. In this
-# case, the "books" query returns an array of zero or more Books (defined above).
-type Query {
-  books: [Book]
-}
+    type Query {
+        myTaskLists: [TaskList!]!
+    }
+    type Mutation {
+        signUp(input: SignUpInput): AuthUser!
+        signIn(input: SignInInput): AuthUser!
+    }
+    input SignUpInput {
+        email: String!
+        password: String!
+        name: String!
+        avtar: String
+    }
+    input SignInInput {
+        email: String!
+        password: String!
+    }
+    type AuthUser {
+        user: User!
+        token: String!
+    }
+    type User {
+        id: ID!
+        name: String!
+        email: String!
+        avatar: String
+    }
+    type TaskList {
+        id: ID!
+        createdAt: String!
+        title: String!
+        progress: Float!
 
-# This "Book" type defines the queryable fields for every book in our data source.
-type Book {
-  title: String
-  author: String
-}
+        users: [User!]!
+        todos: [ToDo!]!
+    }
+    type ToDo {
+        id: ID!
+        content: String!
+        isCompleted: Boolean!
+
+        taskList: TaskList!
+    }
 `;
 
 // Resolvers define the technique for fetching the types defined in the
 // schema. This resolver retrieves books from the "books" array above.
 const resolvers = {
     Query: {
-      books: () => books,
+     myTaskLists: () => []
     },
-  };
+    Mutation: {
+        signUp: async (_,{input}, {db}) => {
+            const hashedPassword = bcrypt.hashSync(input.password);
+            const newUser = {
+                ...input,
+                password: hashedPassword
+            }
+            const result = await db.collection('Users').insertOne(newUser);
+            const user = result.ops[0]
+            return {
+                user,
+                token: 'TOKEN'
+            }
+            console.log(result);
+        },
+        signIn: () => {
+
+        }
+
+    },
+    User: {
+        id: ({ _id, id }) => _id || id
+    }
+  }
+
 const start = async () => {
     const client = new MongoClient(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
   await client.connect();
   const db = client.db(DB_NAME);
   console.log(`Database: ${DB_NAME} Connection`)
+  const context = {
+      db
+  }
  
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({ typeDefs, resolvers, context });
 
 // The `listen` method launches a web server.
 server.listen().then(({ url }) => {
